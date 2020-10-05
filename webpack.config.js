aeae27c373a7b273da6cc540cab39310075c03ff
theS1ngular <hamed.ismaeal1995@gmail.com>
2020-10-05 14:02:18 +0200

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + 'public',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      } 
+    ]   
+ }
+};
