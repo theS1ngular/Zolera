f3ba232eb18c56a89268206af0acc0be00e284dd
hamed ismaeal <hamed.ismaeal1995@gmail.com>
2021-12-30 06:03:07 +0200

    start
    new

--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -5,7 +5,7 @@ export class UserService {
 constructor(private http: HttpClient) {}
 
 login(email: string, password: string) {
-  const user: User= { _id: null, email: email, password: password }
+  const user: User = { _id: null, email, password };
   this.http.post('http://localhost/login', user)
   .subscribe(() => {
     console.log('success');
@@ -13,7 +13,7 @@ login(email: string, password: string) {
 }
 
 register(email: string, password: string) {
-  const user: User= { _id: null, email: email, password: password }
+  const user: User = { _id: null, email, password };
   this.http.post('http://localhost/signup', user)
   .subscribe(() => {
     console.log('hello win');
