6fc494541a52b43d3b4f034bc6663b47987485e3
hamed ismaeal <hamed.ismaeal1995@gmail.com>
2022-01-06 03:24:09 +0200

    updating package

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const http = require("http");
 app.use(express.static(__dirname + '/src'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join(__dirname+'/src/index.html'));
+  res.sendFile(path.join(__dirname+'/src/index.html'));
 });
 
 const normalizePort = val => {
