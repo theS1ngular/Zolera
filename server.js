a2f787bf417a910bd0f1acf430a057ccb118942a
hamed ismaeal <hamed.ismaeal1995@gmail.com>
2022-01-06 03:48:51 +0200

    update server

--- server.js
+++ server.js
@@ -1,60 +1,62 @@
-const app = require("./backend/app");
 const express = require("express");
+const app = require("./backend/app");
+const path = require('path');
 const debug = require("debug")("node-angular");
 const http = require("http");
 
 
-app.use(express.static(__dirname + '/src'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+'/src/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
-const normalizePort = val => {
-  var port = parseInt(val, 10);
+app.listen(process.env.PORT || 8080 );
+// const normalizePort = val => {
+//   var port = parseInt(val, 10);
 
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
+//   if (isNaN(port)) {
+//     // named pipe
+//     return val;
+//   }
 
-  if (port >= 0) {
-    // port number
-    return port;
-  }
+//   if (port >= 0) {
+//     // port number
+//     return port;
+//   }
 
-  return false;
-};
+//   return false;
+// };
 
-const onError = error => {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  switch (error.code) {
-    case "EACCES":
-      console.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      console.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
+// const onError = error => {
+//   if (error.syscall !== "listen") {
+//     throw error;
+//   }
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   switch (error.code) {
+//     case "EACCES":
+//       console.error(bind + " requires elevated privileges");
+//       process.exit(1);
+//       break;
+//     case "EADDRINUSE":
+//       console.error(bind + " is already in use");
+//       process.exit(1);
+//       break;
+//     default:
+//       throw error;
+//   }
+// };
 
-const onListening = () => {
-  const addr = server.address();
-  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
-  debug("Listening on " + bind);
-};
+// const onListening = () => {
+//   const addr = server.address();
+//   const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+//   debug("Listening on " + bind);
+// };
 
-const port = normalizePort(process.env.PORT || 80);
-app.set("port", port);
+// const port = normalizePort(process.env.PORT || 80);
+// app.set("port", port);
 
-const server = http.createServer(app);
-server.on("error", onError);
-server.on("listening", onListening);
-server.listen(port);
+// const server = http.createServer(app);
+// server.on("error", onError);
+// server.on("listening", onListening);
+// server.listen(port);
