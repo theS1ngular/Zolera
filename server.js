0cc5937a9df26919ef2142d8740bd5e299ccf3fb
hamed ismaeal <hamed.ismaeal1995@gmail.com>
2022-01-07 01:06:25 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = require("./backend/app");
+const app = express();
 const path = require('path');
 const debug = require("debug")("node-angular");
 const http = require("http");
